b353ee9f5072c39e481c84fc7c0d8dac1757ab61
Ruslan Dunets <dunecruslan2@gmail.com>
2024-01-24 13:32:56 +0200

    added adaptive for products

--- js/main.js
+++ js/main.js
@@ -32,23 +32,23 @@ for (let hint of infoHints) {
 // Swiper slider
 const swiper = new Swiper('.swiper', {
 	breakpoints: {
-		640: {
+		600: {
 			slidesPerView: 2,
 			spaceBetween: 20,
 		},
-		768: {
-			slidesPerView: 4,
-			spaceBetween: 40,
+		920: {
+			slidesPerView: 3,
+			spaceBetween: 20,
 		},
-		1024: {
-			slidesPerView: 5,
-			spaceBetween: 50,
+		1230: {
+			slidesPerView: 4,
+			spaceBetween: 42,
 		},
 	},
 	direction: 'horizontal',
 	// loop: true,
 	// freeMode: true,
-	slidesPerView: 4,
+	slidesPerView: 1,
 	spaceBetween: 38,
 
 	// Navigation arrows
